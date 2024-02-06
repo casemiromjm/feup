6e1e93466ea48b9fcd8527e982be587b8d409dd7
Casemiro Melo Jorge <88495852+casemiromjm@users.noreply.github.com>
2024-02-06 10:50:43 +0000

    new semester, new language!

---
+++ prog/calculate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, temp_var;
+
+    cin >> x;
+    temp_var = x + 2;
+
+    cout << temp_var * temp_var << "\n";
+
+    return 0;
+}
---
+++ prog/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello! That's my first program in C++\n";
+    return 0;
+}
